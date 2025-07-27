633ce3beafec53b6fd8ded36e43e03c9b3d53cfd
Do Tien Phuoc Quy <dotienphuocquy@gmail.com>
2025-07-27 17:08:17 +0700

    Chapter 9

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,15 +9,6 @@ class User < ApplicationRecord
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-  def self.digest string
-    cost = if ActiveModel::SecurePassword.min_cost
-             BCrypt::Engine::MIN_COST
-           else
-             BCrypt::Engine.cost
-           end
-    BCrypt::Password.create string, cost: cost # rubocop:disable Style/HashSyntax
-  end
-
   before_save :downcase_email
 
   validates :name, presence: true,
@@ -29,6 +20,36 @@ class User < ApplicationRecord
   validates :birthday, presence: true
   validate :birthday_within_max_age
   validates :gender, presence: true
+
+  attr_accessor :remember_token
+
+  def remember
+    self.remember_token = User.new_token
+    update_column :remember_digest, User.digest(remember_token)
+  end
+
+  def forget
+    update_column :remember_digest, nil
+  end
+
+  def authenticated? remember_token
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
+
+  class << self
+    def digest string
+      cost = if ActiveModel::SecurePassword.min_cost
+               BCrypt::Engine::MIN_COST
+             else
+               BCrypt::Engine.cost
+             end
+      BCrypt::Password.create string, cost: cost # rubocop:disable Style/HashSyntax
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
 
   private
 
